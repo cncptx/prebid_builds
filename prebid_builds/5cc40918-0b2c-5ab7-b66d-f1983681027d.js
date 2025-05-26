8d41facdb089f4e6eadd6be58e4be842975009bc
Central <emil@concept.dk>
2025-05-26 13:35:56 +0200

    Updating prebid file prebid_builds/5cc40918-0b2c-5ab7-b66d-f1983681027d.js

--- prebid_builds/5cc40918-0b2c-5ab7-b66d-f1983681027d.js
+++ prebid_builds/5cc40918-0b2c-5ab7-b66d-f1983681027d.js
@@ -1,5 +1,5 @@
 /* prebid.js v9.44.1
-Updated: 2025-05-23
+Updated: 2025-05-26
 Modules: userId, allowActivities, currency, priceFloors, schain, gptPreAuction, adfBidAdapter, sharedIdSystem, unifiedIdSystem, id5IdSystem */
 if(window.pbjs&&window.pbjs.libLoaded)try{window.pbjs.getConfig("debug")&&console.warn("Attempted to load a copy of Prebid.js that clashes with the existing 'pbjs' instance. Load aborted.")}catch(e){}else (function(){
 (()=>{var r,t={70433:(r,t,e)=>{function n(r,t,e,n,o){for(t=t.split?t.split("."):t,n=0;n<t.length;n++)r=r?r[t[n]]:o;return r===o?e:r}e.d(t,{A:()=>n})},68128:r=>{
