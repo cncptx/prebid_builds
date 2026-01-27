1f928307677bf19c3cef69111b7a1654cbe3f6ce
Central <emil@concept.dk>
2026-01-27 12:36:57 +0100

    Updating prebid file prebid_builds/76c9ceab-c693-5247-9ced-82533b091493.js

--- prebid_builds/76c9ceab-c693-5247-9ced-82533b091493.js
+++ prebid_builds/76c9ceab-c693-5247-9ced-82533b091493.js
@@ -1,5 +1,5 @@
 /* prebid.js v10.22.0-pre
-Updated: 2026-01-26
+Updated: 2026-01-27
 Modules: allowActivities, gptPreAuction, consentManagementTcf, tcfControl, gamAdServerVideo, videoModule, rubiconBidAdapter, adfBidAdapter, ixBidAdapter, smartadserverBidAdapter, pubmaticBidAdapter, appnexusBidAdapter */
 if(window.pbjs&&window.pbjs.libLoaded)try{window.pbjs.getConfig("debug")&&console.warn("Attempted to load a copy of Prebid.js that clashes with the existing 'pbjs' instance. Load aborted.")}catch(e){}else (function(){
 (()=>{var t,e={23715(t,e,r){r.d(e,{A:()=>n});const n={pbGlobal:"pbjs",defineGlobal:!0,features:{NATIVE:!0,VIDEO:!0,UID2_CSTG:!0,GREEDY:!1,AUDIO:!0,LOG_NON_ERROR:!0,LOG_ERROR:!0},distUrlBase:"https://cdn.jsdelivr.net/npm/prebid.js@latest/dist/chunks/",skipCalls:{}}},35481(t){
