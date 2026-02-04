df992c3e472c70d8ae9794dfcd7a44bae89a16a0
Central <emil@concept.dk>
2026-02-04 12:35:26 +0100

    Updating prebid file prebid_builds/d4c45a9e-2893-5520-89c6-a88a57161c4d.js

--- prebid_builds/d4c45a9e-2893-5520-89c6-a88a57161c4d.js
+++ prebid_builds/d4c45a9e-2893-5520-89c6-a88a57161c4d.js
@@ -1,5 +1,5 @@
 /* prebid.js v10.18.0
-Updated: 2026-02-03
+Updated: 2026-02-04
 Modules: userId, allowActivities, currency, priceFloors, gptPreAuction, consentManagementTcf, tcfControl, adpod, koblerBidAdapter, appnexusBidAdapter, adfBidAdapter, adnuntiusBidAdapter, pubmaticBidAdapter, rubiconBidAdapter, improvedigitalBidAdapter, sharedIdSystem, unifiedIdSystem, id5IdSystem */
 if(window.pbjs&&window.pbjs.libLoaded)try{window.pbjs.getConfig("debug")&&console.warn("Attempted to load a copy of Prebid.js that clashes with the existing 'pbjs' instance. Load aborted.")}catch(e){}else (function(){
 (()=>{var t,r={23715:(t,r,e)=>{e.d(r,{A:()=>n});const n={pbGlobal:"pbjs",defineGlobal:!0,features:{NATIVE:!0,VIDEO:!0,UID2_CSTG:!0,GREEDY:!1,AUDIO:!0,LOG_NON_ERROR:!0,LOG_ERROR:!0},distUrlBase:"https://cdn.jsdelivr.net/npm/prebid.js@10.18.0/dist/chunks/",skipCalls:{}}},35481:t=>{
